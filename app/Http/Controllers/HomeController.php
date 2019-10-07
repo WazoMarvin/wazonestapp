775f757fc47942ad2ec63440abb5046c7e29c3a7
unknown <marvin.kiseka@trailanalytics.com>
2019-10-07 17:12:48 +0300

    front

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,11 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
+    }
+
+    public function about(){
+        return view('marvin');
     }
 
     /**
